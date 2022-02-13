104574a79cdbae759da685322416ea60f63f3ec1
steventu@wezhuiyi.com <2295527839@qq.com>
2022-02-13 22:56:09 +0800

    feat: modify some config

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './index.css';
+// TODO: 试着换成scss
+
+const Home: React.FunctionComponent = () => {
+    return (
+    <div className='ht'>ss</div>
+    );
+}
+
+export default Home;
